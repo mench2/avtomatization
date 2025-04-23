ace5e755d2d25b3519e90928003162a8750b23d4
mench2 <130401925+mench2@users.noreply.github.com>
2025-04-23 17:49:08 +0300

    ver1.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          вавапва.
+          <h1>Hello World</h1>
         </p>
         <a
           className="App-link"
